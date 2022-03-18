e1d44994fd1f99e67cf7d16b82cf2b5be360ffdb
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-03-18 21:26:39 +0000

    Plateau Interface written

---
+++ src/ts/interfaces/Plateau.interface.ts
@@ -0,0 +1,9 @@
+export interface PlateauInterface {
+    maxXCoord: number
+    maxYCoord : number
+    maxZCoord? : number
+    
+    getPlateauCoords() : string
+
+    setPlateauCoords(newXCoord: number, newYCoord : number, newZCoord? : number) : string
+}
