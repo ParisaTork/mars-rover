03915177a5910af57b54de3ab27076b38901322d
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-03-18 21:28:27 +0000

    Vehicle Interface written

---
+++ src/ts/interfaces/Vehicle.interface.ts
@@ -0,0 +1,37 @@
+import { OrientationType } from "../types/types"
+
+export interface VehicleInterface {
+    startXCoord: number
+    startYCoord: number
+    startZCoord?: number
+    startOrient: OrientationType
+    currentXCoord: number
+    currentYCoord: number
+    currentZCoord?: number
+    currentOrient: OrientationType
+    finalXCoord: number
+    finalYCoord: number
+    finalZCoord?: number
+    finalOrient: OrientationType
+    
+    spinLeft() : OrientationType
+
+    spinRight() : OrientationType
+
+    moveForwardByOne() : number
+
+    getCurrentCoords() : String
+
+    setCurrentCoords(newXCoord: number, newYCoord : number, newZCoord? : number) : string
+
+    getCurrentOrientation() : String
+
+    setCurrentOrientation(newOrient : OrientationType) : String
+
+    runInstructions(str : string) : string
+
+    isClash(newXCoord: number, newYCoord: number) : boolean
+
+    isOutBound(newXCoord: number, newYCoord: number): boolean
+
+}
