636a1f17ca32993339560dd39a5f9d21c4b0c537
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-03-18 21:33:00 +0000

    Plateau class instantiation rightly passes

---
+++ src/ts/classes/Plateau.ts
@@ -0,0 +1,9 @@
+import { PlateauInterface } from '../interfaces/Plateau.interface';
+export class Plateau implements PlateauInterface {
+    maxXCoord : number;
+    maxYCoord: number;
+    constructor(maxXCoord: number, maxYCoord : number) {
+        this.maxXCoord = maxXCoord
+        this.maxYCoord = maxYCoord
+    }
+}
