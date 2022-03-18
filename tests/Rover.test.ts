24f3544a981b5bffc0dd46dd038bf28f7aa935a8
ParisaTork <47482049+ParisaTork@users.noreply.github.com>
2022-03-18 21:41:51 +0000

    Test for Rover to spin left rightly fails

--- tests/Rover.test.ts
+++ tests/Rover.test.ts
@@ -9,3 +9,19 @@ import {Plateau} from '../src/ts/classes/Plateau'
       expect(newInstanceOfRover.startOrient).toEqual('W');
     });
   });
+
+  describe('Test for Rover Orientation', () => {
+    it('should be able to spin left', () => {
+      const newInstanceOfRover = new Rover(3,3,'N');
+      newInstanceOfRover.spinLeft(); 
+      expect(newInstanceOfRover.currentOrient).toEqual('W');
+      const newInstanceOfRover2 = new Rover(3,3,'E');
+      newInstanceOfRover2.spinLeft(); 
+      expect(newInstanceOfRover2.currentOrient).toEqual('N');
+      const newInstanceOfRover3 = new Rover(3,3,'W');
+      newInstanceOfRover3.spinLeft(); 
+      expect(newInstanceOfRover3.currentOrient).toEqual('S');
+      const newInstanceOfRover4 = new Rover(3,3,'S');
+      newInstanceOfRover4.spinLeft(); 
+      expect(newInstanceOfRover4.currentOrient).toEqual('E');
+    });
